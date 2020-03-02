80fc898e77e78d39e8085a27296420e341e63a1c
arunpanwar5 <61677278+arunpanwar5@users.noreply.github.com>
2020-03-03 00:48:04 +0530

    Add files via upload

---
+++ Capstone Project Notebook.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <a href="https://www.bigdatauniversity.com"><img src="https://ibm.box.com/shared/static/cw2c7r3o20w9zn8gkecaeyjhgw3xdgbj.png" width="400" align="center"></a>
+# 
+# <h1 align="center"><font size="5">Capstone Project Notebook</font></h1>
+
+# This notebook will be mainly used for the capstone project.
+# 
+# We load a dataset using Pandas library, and apply the following algorithms, and find the best one for this specific dataset by accuracy evaluation methods.
+# 
+# Lets first load required libraries:
+
+# In[1]:
+
+
+import numpy as np
+import numpy as np
+
+
+# Hello Capstone Project Course!
+
+# In[ ]:
+
+
+
+
